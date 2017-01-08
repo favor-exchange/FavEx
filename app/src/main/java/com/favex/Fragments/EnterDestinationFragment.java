d01402fe00ff621c86865c5175934fb71c9f3ab0
tavishcode <tavish.gobindram@gmail.com>
2017-01-09 05:33:13 +0700

    fixed permission bugs

--- app/src/main/java/com/favex/Fragments/EnterDestinationFragment.java
+++ app/src/main/java/com/favex/Fragments/EnterDestinationFragment.java
@@ -6,6 +6,7 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -30,36 +31,49 @@ import com.google.android.gms.location.places.Places;
 public class EnterDestinationFragment extends Fragment {
     private Button mCurrentLocationBtn;
     private GoogleApiClient mGoogleApiClient;
-    private static final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION=1;
+    private static final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view= inflater.inflate(R.layout.enter_destination_fragment,container,false);
-        mGoogleApiClient=((FavorFormActivity)getActivity()).getGoogleApiClient();
+        View view = inflater.inflate(R.layout.enter_destination_fragment, container, false);
+        mGoogleApiClient = ((FavorFormActivity) getActivity()).getGoogleApiClient();
         // Would be more efficient to implement Google API Client as singleton, but currently not a priority
-        mCurrentLocationBtn= (Button)view.findViewById(R.id.currentLocationBtn);
+        mCurrentLocationBtn = (Button) view.findViewById(R.id.currentLocationBtn);
         mCurrentLocationBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
-                        != PackageManager.PERMISSION_GRANTED)
-                {
-                    ActivityCompat.requestPermissions(getActivity(),
-                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                             MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+                }
+                else{
+                    PendingResult<PlaceLikelihoodBuffer> result = Places.PlaceDetectionApi.getCurrentPlace(mGoogleApiClient, null);
+                    result.setResultCallback(new ResultCallback<PlaceLikelihoodBuffer>() {
+                        @Override
+                        public void onResult(PlaceLikelihoodBuffer likelyPlaces) {
+                            for (PlaceLikelihood placeLikelihood : likelyPlaces) {
+                                Log.i("CURRENT PLACE", String.format("Place '%s' has likelihood: %g",
+                                        placeLikelihood.getPlace().getName(),
+                                        placeLikelihood.getLikelihood()));
+                            }
+                            likelyPlaces.release();
+                        }
+                    });
                 }
             }
         });
         return view;
     }
+
     @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         switch (requestCode) {
             case MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                Log.i("Permissions","Permission interaction!");
                 // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    PendingResult<PlaceLikelihoodBuffer> result = Places.PlaceDetectionApi
-                            .getCurrentPlace(mGoogleApiClient, null);
+                if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED) {
+                    PendingResult<PlaceLikelihoodBuffer> result = Places.PlaceDetectionApi.getCurrentPlace(mGoogleApiClient, null);
                     result.setResultCallback(new ResultCallback<PlaceLikelihoodBuffer>() {
                         @Override
                         public void onResult(PlaceLikelihoodBuffer likelyPlaces) {
