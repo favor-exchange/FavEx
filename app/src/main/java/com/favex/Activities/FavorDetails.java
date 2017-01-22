d92348cea2eb011769bcf0f1d09fad5a61e39427
tavishcode <tavish.gobindram@gmail.com>
2017-01-22 16:57:24 +0700

    gallery adapter

---
+++ app/src/main/java/com/favex/Activities/FavorDetails.java
@@ -0,0 +1,8 @@
+package com.favex.Activities;
+
+/**
+ * Created by Tavish on 21-Jan-17.
+ */
+
+public class FavorDetails {
+}
---
+++ app/src/main/java/com/favex/Adapters/galleryAdapter.java
@@ -0,0 +1,8 @@
+package com.favex.Adapters;
+
+/**
+ * Created by Tavish on 22-Jan-17.
+ */
+
+public class galleryAdapter {
+}
